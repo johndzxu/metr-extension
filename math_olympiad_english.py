3372a42b6558b35190ea3e01a0ba9ec69eb4f06d
MACARRILLO <ma.carrillo.cobian@gmail.com>
2025-12-01 18:08:58 -0500

    results

--- math_olympiad_english.py
+++ math_olympiad_english.py
@@ -169,7 +169,7 @@ def math_olympiad_english():
     subset = select_by_T_human_bins(dataset, n_bins=10, per_bin=5, log_space=True)
     return Task(
         dataset=subset,
-        #solver=agent(),
+        solver=[ prompt_template(MATH_PROMPT_TEMPLATE), generate(config=GenerateConfig(temperature=0.9))], 
         scorer=olympiadbench_scorer(),
         #sandbox="docker",
         epochs=Epochs(4, [mean_score(), pass_at(1)]),
